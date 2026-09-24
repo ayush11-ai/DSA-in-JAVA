8486dbfb1fbc7904d2001864216dd777f3662541
ayush11-ai <nikamayush82@gmail.com>
2026-09-24 21:01:03 +0530

    Time: 4 ms (15.99%), Space: 44.1 MB (16.27%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,15 +1,14 @@
 import java.util.*;
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int left = 0;
 
-        for(int i=m ; i<m+n ; i++){
-           nums1[i]=nums2[left];
-           left++;
-        }
+     for(int i=0 ; i<n ; i++){
+        nums1[m+i]=nums2[i];
+     } 
 
-        Arrays.sort(nums1);
+     Arrays.sort(nums1);
 
+    // System.out.print(Arrays.toString(nums1));
 
     }
 }
